cf89a177d7a838f95732f95717d6ec91e71b9c13
prajwalraju <prajwalrajup@gmail.com>
2021-10-03 02:52:46 +0530

    final bug fix hopefully

--- netlify_function/cors.js
+++ netlify_function/cors.js
@@ -1,49 +1,104 @@
-import fetch from "node-fetch";
+var axios = require("axios");
+
 exports.handler = async (event, context) => {
-	
-	var url = event.path;
-	url = url.split(".netlify/functions/cors/")[1];
-	url = decodeURIComponent(url);
-	url = new URL(url);
-	
-	for (let i in event.queryStringParameters) {
-		url.searchParams.append(i, event.queryStringParameters[i]);
-	}
-	console.log(url.href);
-	var cookie_string = event.headers.cookie || "";
-	var useragent = event.headers["user-agent"] || "";
-	
-	var header_to_send= {
-		"Cookie": cookie_string,
-		"User-Agent": useragent,
-		"content-type": "application/json",
-		"accept": "*/*",
-		"host": url.host
-	};
-	
-	var options = {
-		method: event.httpMethod.toUpperCase(),
-		headers: header_to_send,
-		body: event.body
-	}
-	
-	if (event.httpMethod.toUpperCase() == "GET" || event.httpMethod.toUpperCase() == "HEAD") delete options.body;
-	
-	var response = await fetch(url, options);
-	var response_text = await response.text();
-	var headers = response.headers.raw();
-	
-	var cookie_header = null;
-	if (headers["set-cookie"]) cookie_header = headers["set-cookie"];
-	
-	return {
-		statusCode: 200,
-		body: response_text,
-		headers: {
-			"content-type": String(headers["content-type"]) || "text/plain"
-		},
-		multiValueHeaders: {
-			"set-cookie": cookie_header || []
-		}
-	}
+  var url = event.path;
+  url = url.split(".netlify/functions/cors/")[1];
+  url = decodeURIComponent(url);
+  url = new URL(url);
+
+  for (let i in event.queryStringParameters) {
+    url.searchParams.append(i, event.queryStringParameters[i]);
+  }
+
+  console.log(url.href);
+  var cookie_string = event.headers.cookie || "";
+  var useragent = event.headers["user-agent"] || "";
+
+  var header_to_send = {
+    Cookie: cookie_string,
+    "User-Agent": useragent,
+    "content-type": "application/json",
+    accept: "*/*",
+    host: url.host,
+  };
+
+  var options = {
+    method: event.httpMethod.toUpperCase(),
+    headers: header_to_send,
+    body: event.body,
+  };
+
+  if (
+    event.httpMethod.toUpperCase() == "GET" ||
+    event.httpMethod.toUpperCase() == "HEAD"
+  )
+    delete options.body;
+
+  const request = {
+    method: event.httpMethod.toUpperCase(),
+    url: url.href,
+    headers: header_to_send,
+    proxy: {
+      host: "137.116.150.50",
+      port: 3128,
+    },
+  };
+
+  const res = await axios(request);
+  console.log("!!!!!!");
+  console.log(res.data);
+  return {
+    statusCode: 200,
+    body: await JSON.stringify(res.data),
+    headers: {
+      "content-type": "application/json",
+    },
+  };
+
+  // var url = event.path;
+  // url = url.split(".netlify/functions/cors/")[1];
+  // url = decodeURIComponent(url);
+  // url = new URL(url);
+
+  // for (let i in event.queryStringParameters) {
+  // 	url.searchParams.append(i, event.queryStringParameters[i]);
+  // }
+
+  // console.log(url.href);
+  // var cookie_string = event.headers.cookie || "";
+  // var useragent = event.headers["user-agent"] || "";
+
+  // var header_to_send= {
+  // 	"Cookie": cookie_string,
+  // 	"User-Agent": useragent,
+  // 	"content-type": "application/json",
+  // 	"accept": "*/*",
+  // 	"host": url.host
+  // };
+
+  // var options = {
+  // 	method: event.httpMethod.toUpperCase(),
+  // 	headers: header_to_send,
+  // 	body: event.body
+  // }
+
+  // if (event.httpMethod.toUpperCase() == "GET" || event.httpMethod.toUpperCase() == "HEAD") delete options.body;
+
+  // var response = await fetch(url, options);
+  // var response_text = await response.text();
+  // var headers = response.headers.raw();
+
+  // var cookie_header = null;
+  // if (headers["set-cookie"]) cookie_header = headers["set-cookie"];
+
+  // return {
+  // 	statusCode: 200,
+  // 	body: response_text,
+  // 	headers: {
+  // 		"content-type": String(headers["content-type"]) || "text/plain"
+  // 	},
+  // 	multiValueHeaders: {
+  // 		"set-cookie": cookie_header || []
+  // 	}
+  // }
 };
